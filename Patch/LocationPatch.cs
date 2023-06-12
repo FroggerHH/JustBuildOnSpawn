9032c9adb1a610aef188de415432bd873ade64a3
Vyacheslav <slava.shilin07@mail.ru>
2023-06-12 20:13:05 +0300

    Initial commit

---
+++ Patch/LocationPatch.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx.Bootstrap;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using static JustBuildOnSpawn.Plugin;
+
+namespace JustBuildOnSpawn;
+
+[HarmonyPatch]
+internal class LocationPatch
+{
+    [HarmonyPatch(typeof(Location), nameof(Location.IsInsideNoBuildLocation)), HarmonyPrefix]
+    public static bool ChatAddSMode(Chat __instance, ref bool __result)
+    {
+        __result = false;
+        return false;
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,65 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace JustBuildOnSpawn;
+
+[BepInPlugin(ModGUID, ModName, ModVersion)]
+internal class Plugin : BaseUnityPlugin
+{
+    #region values
+
+    internal const string ModName = "JustBuildOnSpawn", ModVersion = "1.0.0", ModGUID = "com.Frogger." + ModName;
+    internal static Harmony harmony = new(ModGUID);
+
+    internal static Plugin _self;
+
+    #endregion
+    
+    #region tools
+
+    public static void Debug(string msg, bool localize = false)
+    {
+        if (Localization.instance != null && localize)
+        {
+            _self.Logger.LogInfo(Localization.instance.Localize(msg));
+        }
+        else
+        {
+            _self.Logger.LogInfo(msg);
+        }
+    }
+
+    public void DebugError(string msg, bool showWriteToDev)
+    {
+        if (showWriteToDev)
+        {
+            msg += "Write to the developer and moderator if this happens often.";
+        }
+
+        Logger.LogError(msg);
+    }
+
+    public void DebugWarning(string msg, bool showWriteToDev)
+    {
+        if (showWriteToDev)
+        {
+            msg += "Write to the developer and moderator if this happens often.";
+        }
+
+        Logger.LogWarning(msg);
+    }
+
+    #endregion
+
+    private void Awake()
+    {
+        _self = this;
+        
+        harmony.PatchAll();
+    }
+}
